a67619164dd8673bf2da7936261e575d08ff2b73
Manabu MABUCHI <mabmabgg@gmail.com>
2020-07-01 15:44:14 +0900

    /usersのGET要求でRaspberryPiのCPU温度を返すようにした

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
+const { execSync } = require('child_process');
+
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  const temp = execSync('vcgencmd measure_temp | sed -e "s/temp=//" | sed -z "s/\'C\\n//"');
+  console.log(temp.toString());
+  res.json({
+    temperature: temp.toString()
+  })
 });
 
 module.exports = router;
