81be464502de17e701e32878f5622df01022e854
Manabu MABUCHI <mabmabgg@gmail.com>
2020-07-01 15:31:24 +0900

    ホームページにサーバーの起動時間を表示するようにした

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
+const { execSync } = require('child_process');
+
 var router = express.Router();
+
+/* GET Date Time Now */
+const dt = execSync('date');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', started_time: dt });
 });
 
 module.exports = router;
